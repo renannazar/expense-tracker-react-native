a94689a684799fddd70729a0369c46b8535496f4
Renan Nazar <renan.nazar123@gmail.com>
2023-11-01 17:08:03 +0700

    Backup and restore functionality

--- screens/SettingScreen.tsx
+++ screens/SettingScreen.tsx
@@ -1,10 +1,82 @@
 import React from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Alert,
+  DevSettings,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {GreenColor, PrimaryColor} from '../utils/Color';
 import {GlobalStyles} from '../styles';
+import RNFS from 'react-native-fs';
+import moment from 'moment';
+import DocumentPicker from 'react-native-document-picker';
 
 function SettingScreen() {
+  const backupData = async () => {
+    try {
+      var libDirectory = RNFS.DocumentDirectoryPath.replace(
+        'files',
+        'databases',
+      );
+
+      // const database = await RNFS.readFile(`${libDirectory}/keuangan.db`);
+      const targetBackup =
+        RNFS.DownloadDirectoryPath +
+        '/keuangan_tracker_' +
+        moment().unix() +
+        '.db';
+      // RNFS.writeFile(targetBackup, database, 'utf8');
+      await RNFS.copyFile(libDirectory + '/keuangan.db', targetBackup);
+
+      const readDownload = await RNFS.readDir(RNFS.DownloadDirectoryPath);
+      console.log(readDownload);
+      Alert.alert('Berhasil Backup Database', `Tersimpan di ${targetBackup}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const restoreData = async () => {
+    Alert.alert(
+      'Apakah anda yakin?',
+      'Dengan melakukan Restore, data sebelumnya akan dihapus dan digantikan data yang baru',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Lanjutkan',
+          onPress: pickFileRestore,
+        },
+      ],
+    );
+  };
+
+  const pickFileRestore = async () => {
+    try {
+      const pickerResult = await DocumentPicker.pickSingle({
+        presentationStyle: 'fullScreen',
+        copyTo: 'cachesDirectory',
+      });
+      if (pickerResult != null) {
+        const decodeResult = pickerResult.uri;
+        const targetResult =
+          RNFS.DocumentDirectoryPath.replace('files', 'databases') +
+          '/keuangan.db';
+
+        await RNFS.copyFile(decodeResult, targetResult);
+
+        DevSettings.reload();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <View style={styles.container}>
@@ -22,10 +94,10 @@ function SettingScreen() {
           data terlebih dahulu
         </Text>
 
-        <TouchableOpacity style={styles.btnBackup}>
+        <TouchableOpacity style={styles.btnBackup} onPress={backupData}>
           <Text style={GlobalStyles.colorWhite}>Backup Data</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.btnRestore}>
+        <TouchableOpacity style={styles.btnRestore} onPress={restoreData}>
           <Text style={GlobalStyles.colorWhite}>Restore Data</Text>
         </TouchableOpacity>
       </View>
